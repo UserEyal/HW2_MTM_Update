8d908781c4a7aefb6031a93234c0d8127d0fdebc
UserEyal <149259434+UserEyal@users.noreply.github.com>
2025-11-08 22:12:52 +0200

    Use cleaner exception handling

--- wet/Utilities.h
+++ wet/Utilities.h
@@ -1,10 +1,14 @@
 
 #pragma once
 
+#include <stdexcept>
+
 enum class MatamErrorType {
     UnmatchedSizes,
     OutOfBounds,
     NotSquareMatrix
 };
+
+using Exit = std::runtime_error;
 
 void exitWithError(MatamErrorType error);
